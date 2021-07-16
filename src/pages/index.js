9c09da293a70cf4c6ad3d492e6f210e729e01796
Rob Clark <rob@robclark.io>
2021-07-16 03:10:34 -0500

    Fixed logo path.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-		<img src="/static/img/front-logo.svg" id="front-logo" width="410" />
+		<img src="/method-wiki/img/front-logo.svg" id="front-logo" width="410" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
